2d27ec9373b6d3e250da2eab4523ccf6bcdb15a1
Чичканов Дмитрий Владимирович <chdmitr@skbkontur.ru>
2022-11-27 19:39:13 +0500

    linter fix

--- fail2ban-exporter.py
+++ fail2ban-exporter.py
@@ -1,4 +1,3 @@
-import importlib
 import yaml
 import configparser
 import sqlite3
@@ -20,8 +19,8 @@ class Jail:
 class F2bCollector(object):
     def __init__(self, conf):
         self.geo_provider = self._import_provider(conf)
-        self.f2b_conf = conf['f2b'].get('conf','')
-        self.f2b_conf_path = conf['f2b'].get('conf_path','')
+        self.f2b_conf = conf['f2b'].get('conf', '')
+        self.f2b_conf_path = conf['f2b'].get('conf_path', '')
         self.f2b_db = conf['f2b']['db']
         self.jails = []
         self.extra_labels = sorted(self.geo_provider.get_labels())
@@ -29,7 +28,8 @@ class F2bCollector(object):
     def _import_provider(self, conf):
         if conf['geo']['enabled']:
             class_name = conf['geo']['provider']
-            mod = __import__('geoip_provider.{}'.format(class_name.lower()), fromlist=[class_name])
+            mod = __import__('geoip_provider.{}'.format(class_name.lower()),
+                             fromlist=[class_name])
         else:
             class_name = 'BaseProvider'
             mod = __import__('geoip_provider.base', fromlist=['BaseProvider'])
@@ -52,10 +52,12 @@ class F2bCollector(object):
             config.read('{}/jail.local'.format(self.f2b_conf_path))
 
         if self.f2b_conf_path:
-            jaild = list(Path('{}/jail.d'.format(self.f2b_conf_path)).glob('*.local'))
+            jaild = list(Path('{}/jail.d'.format(self.f2b_conf_path)).
+                         glob('*.local'))
             config.read(jaild)
 
-        active_jails = cur.execute('SELECT name FROM jails WHERE enabled = 1').fetchall()
+        active_jails = cur.execute('SELECT name FROM jails \
+                                   WHERE enabled = 1').fetchall()
 
         for j in active_jails:
             jail = Jail(j[0])
@@ -64,9 +66,11 @@ class F2bCollector(object):
             self.jails.append(jail)
 
         for jail in self.jails:
-            date_query = f"DATETIME(timeofban + '{jail.bantime}', 'unixepoch') > DATETIME('now') AND" \
-                        if jail.bantime != -1 else ""
-            query = f"SELECT ip,timeofban FROM bips WHERE {date_query} jail = '{jail.name}'"
+            date_query = f"DATETIME(timeofban + '{jail.bantime}', \
+                         'unixepoch') > DATETIME('now') AND" \
+                         if jail.bantime != -1 else ""
+            query = f"SELECT ip,timeofban FROM bips \
+                     WHERE {date_query} jail = '{jail.name}'"
             rows = cur.execute(query).fetchall()
             for row in rows:
                 jail.ip_list.append({'ip': row[0], 'timeofban': row[1] * 1000})
@@ -90,27 +94,34 @@ class F2bCollector(object):
 
     def expose_single(self):
         metric_labels = ['ip', 'jail', 'time_of_ban'] + self.extra_labels
-        gauge = GaugeMetricFamily('fail2ban_banned_ip', 'IP banned by fail2ban', labels=metric_labels)
+        gauge = GaugeMetricFamily('fail2ban_banned_ip',
+                                  'IP banned by fail2ban',
+                                  labels=metric_labels)
 
         for jail in self.jails:
             for entry in jail.ip_list:
-                # Skip if GeoProvider.annotate() did not return matching count of labels
+                # Skip if GeoProvider.annotate()
+                # did not return matching count of labels
                 if len(entry) < len(self.extra_labels) + 1:
                     continue
-                values = [entry['ip'], jail.name, str(entry['timeofban'])] + [ entry[x] for x in self.extra_labels ]
+                values = [entry['ip'], jail.name, str(entry['timeofban'])] + \
+                         [entry[x] for x in self.extra_labels]
                 gauge.add_metric(values, 1)
 
         return gauge
 
     def expose_grouped(self):
-        gauge = GaugeMetricFamily('fail2ban_location', 'Number of currently banned IPs from this location', labels=self.extra_labels)
+        gauge = GaugeMetricFamily('fail2ban_location',
+                                  'Number of currently banned IPs \
+                                      from this location',
+                                  labels=self.extra_labels)
         grouped = defaultdict(int)
 
         for jail in self.jails:
             for entry in jail.ip_list:
                 if not entry:
                     continue
-                location_key = tuple([ entry[x] for x in self.extra_labels ])
+                location_key = tuple([entry[x] for x in self.extra_labels])
                 grouped[location_key] += 1
 
         for labels, count in grouped.items():
@@ -119,7 +130,9 @@ class F2bCollector(object):
         return gauge
 
     def expose_jail_summary(self):
-        gauge = GaugeMetricFamily('fail2ban_jailed_ips', 'Number of currently banned IPs per jail', labels=['jail'])
+        gauge = GaugeMetricFamily('fail2ban_jailed_ips',
+                                  'Number of currently banned IPs per jail',
+                                  labels=['jail'])
 
         for jail in self.jails:
             gauge.add_metric([jail.name], len(jail.ip_list))
@@ -138,8 +151,10 @@ if __name__ == '__main__':
     REGISTRY.register(F2bCollector(conf))
 
     app = make_wsgi_app()
-    print(f"Starting fail2ban exporter. Listen {conf['server']['listen_address']}:{conf['server']['port']}")
+    print("Starting fail2ban exporter.",
+          f"Listen {conf['server']['listen_address']}" +
+          f":{conf['server']['port']}")
     httpd = make_server(host=conf['server']['listen_address'],
-          port=conf['server']['port'],
-          app=app)
+                        port=conf['server']['port'],
+                        app=app)
     httpd.serve_forever()
