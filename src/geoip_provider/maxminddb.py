af339a5dcdeeb93e5dc6924d046c4719b61278b5
chdmitr <dimonstr84@mail.ru>
2026-01-23 11:40:13 +0500

    fix maximinddb exception logic

--- src/geoip_provider/maxminddb.py
+++ src/geoip_provider/maxminddb.py
@@ -4,7 +4,7 @@ import geoip2.database
 class MaxmindDB:
     def __init__(self, config: dict):
         self.db_path = config['geo']['maxmind']['db_path']
-        self.on_error = config['geo']['maxmind'].get('on_error', '')
+        self.on_error = config['geo']['maxmind'].get('on_error', {})
 
     def annotate(self, ip: str) -> dict:
         reader = geoip2.database.Reader(self.db_path)
@@ -15,9 +15,9 @@ class MaxmindDB:
                 latitude=str(lookup.location.latitude),
                 longitude=str(lookup.location.longitude)
             )
-        except Exception:
-            if not self.on_error:
-                entry = {}
+        except Exception as e:
+            if self.on_error is dict and len(self.on_error) > 0:
+                entry = self.on_error
             else:
                 entry = dict(
                     city=self.on_error.get('city', 'not set'),
